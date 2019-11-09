1388ec6268ffac8793061320ece9573a3619fcdf
AdamZ <adam.zobrist@oncorems.com>
2019-11-09 09:09:49 -0600

    quick fix - see .gitfixes .gitfixes

---
+++ server.py
@@ -0,0 +1,18 @@
+# DataServer2.py
+
+from tcpcom import TCPServer
+import time
+
+IP_PORT = 22000
+
+def onStateChanged(state, msg):
+    if state == "LISTENING":
+        print "Server:-- Listening..."
+    elif state == "CONNECTED":
+        print "Server:-- Connected to", msg
+    elif state == "MESSAGE":
+        print "Server:-- Message received:", msg
+        if msg == "go":
+            server.sendMessage("Hello")
+
+server = TCPServer(IP_PORT, stateChanged = onStateChanged)
